80caed50f0f800629dcfa838dc5d2fe3a27d5d9f
BrandonDruch <brandondruch@gmail.com>
2025-06-28 19:33:46 -0400

    Update retrieve.js

--- api/retrieve.js
+++ api/retrieve.js
@@ -1,38 +1,37 @@
-const { send } = require('micro');
-const { URL } = require('url');
+const { URL } = require('url')
 
 module.exports = async (req, res) => {
   try {
     if (req.method !== 'GET') {
-      return send(res, 405, { error: 'Method not allowed' });
+      res.writeHead(405, { 'Content-Type': 'application/json' })
+      return res.end(JSON.stringify({ error: 'Method not allowed' }))
     }
-
-    // parse ?code= from URL
-    const { code } = new URL(req.url, `http://${req.headers.host}`).searchParams;
+    // Parse code from URL
+    const url = new URL(req.url, `http://${req.headers.host}`)
+    const code = url.searchParams.get('code')
     if (!code || !/^[1-9]\d{4}$/.test(code)) {
-      return send(res, 400, { error: 'Invalid code format.' });
+      res.writeHead(400, { 'Content-Type': 'application/json' })
+      return res.end(JSON.stringify({ error: 'Invalid code format.' }))
     }
-
+    // Fetch email
     const upstashRes = await fetch(
       `${process.env.UPSTASH_REDIS_REST_URL}/get/${code}`,
-      {
-        method: 'GET',
-        headers: { Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}` },
-      }
-    );
+      { method: 'GET', headers: { Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}` } }
+    )
     if (!upstashRes.ok) {
-      return send(res, 500, { error: 'Failed to retrieve code.' });
+      throw new Error('Upstash fetch error')
     }
-
-    const data = await upstashRes.json();
-    const email = data.result ? decodeURIComponent(data.result) : null;
+    const data = await upstashRes.json()
+    const email = data.result ? decodeURIComponent(data.result) : null
     if (!email) {
-      return send(res, 404, { error: 'Code expired or not found.' });
+      res.writeHead(404, { 'Content-Type': 'application/json' })
+      return res.end(JSON.stringify({ error: 'Code expired or not found.' }))
     }
-
-    return send(res, 200, { email });
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    return res.end(JSON.stringify({ email }))
   } catch (err) {
-    console.error('retrieve error:', err);
-    return send(res, 500, { error: 'Internal server error.' });
+    console.error('retrieve error', err)
+    res.writeHead(500, { 'Content-Type': 'application/json' })
+    return res.end(JSON.stringify({ error: 'Internal server error.' }))
   }
-};
+}
